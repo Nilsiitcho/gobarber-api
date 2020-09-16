d98058c9d59d6e3975b2dc87d5dc143323ea4274
Nilson <nilson.silva@sankhya.com.br>
2020-09-15 23:36:14 -0300

    Criando rota de sessions

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express, {Router} from 'express';
 import appointmentsRouter from "./appointments.routes";
 import usersRouter from "./users.routes";
+import sessionsRouter from "./sessions.routes";
 
 const routes = Router();
 
@@ -8,5 +9,6 @@ routes.use(express.json());
 routes.use(express.urlencoded({extended: true}));
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
