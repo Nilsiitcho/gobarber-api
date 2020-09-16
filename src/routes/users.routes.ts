108a91c649ffe4cef12c56e88c68beac8cec004b
Nilson <nilson.silva@sankhya.com.br>
2020-09-16 19:54:51 -0300

    Adicionando alteração de avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,6 +4,7 @@ import uploadConfig from "../config/upload";
 
 import CreateUserService from "../services/CreateUserService";
 import ensureAuthenticated from "../middlewares/ensureAuthenticated";
+import UpdateUserAvatarService from "../services/UpdateUserAvatarService";
 
 const usersRouter = Router();
 
@@ -35,9 +36,22 @@ usersRouter.patch(
     ensureAuthenticated,
     upload.single('avatar'),
     async (request, response) => {
-        console.log(request.file);
 
-        return response.json({ok: true});
+        try {
+            const updateUserAvatar = new UpdateUserAvatarService();
+
+            const user = await updateUserAvatar.execute({
+                user_id: request.user.id,
+                avatarFilename: request.file.filename
+            })
+
+            // @ts-ignore
+            delete user.password;
+
+            return response.json(user);
+        } catch (err) {
+            return response.status(400).json({error: err.message});
+        }
     },
 )
 
